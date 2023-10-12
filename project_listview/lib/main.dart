a044d498be748d0115548e2d7e68a4b7a391186a
ZikriatulQalbi <126840470+ZikriatulQalbi@users.noreply.github.com>
2023-10-12 11:42:07 +0700

    Project-ListView

---
+++ project_listview/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Color> myColor = [
+    Colors.brown,
+    Colors.deepPurple,
+    Colors.teal,
+    Colors.yellow
+  ];
+
+  final List<Widget> myList = List.generate(
+    100,
+    (index) => Text(
+      "${index + 1}",
+      style: TextStyle(
+        fontSize: 20 + double.parse(index.toString()),
+      ),
+      //index.toString(), //mulai dari 0
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List View"),
+        ),
+        body: ListView(
+          children: myList,
+        ),
+      ),
+    );
+  }
+}
